45c22b8b3cfcf57e1baf7929003c8844fc8586a5
Psychee <orelydbm@gamil.com>
2022-05-05 16:58:25 +0200

    mise en place des commentaires et suppression de se qui ne sert pas

--- includes/affichageAccueil.php
+++ includes/affichageAccueil.php
@@ -1,59 +1,64 @@
 <?php
 	
-function derniersArtilcesAccueil($atts){
-
-    extract( shortcode_atts( array(
-		'num' => '5',
-		'order' => 'DESC',
-		'orderby' => 'date',
-	), $atts) );
+function derniersArtilcesAccueil(){
 
     $output= "<body>
 	<h2>L'actualité du club</h2>";
 
 	$posts = get_posts();
+	$espaceurTop = '';
+    $indiceEspaceur = 0;
 
+	// on boucle dans les posts
 	foreach ($posts as $post) {
 		$post_id = $post->ID ;
 		$permalink = get_permalink( $post_id );
 
+		// Pas de margin-top sur le premier tableau d'article'
+		if($indiceEspaceur > 0) {
+			$espaceurTop = 'lastPost_margin_top';
+		}
+
+		//on affiche le titre de l'article
 		$output = $output ."<figure>
-		<div class='lastPost_overflow lastPost_max_width lastPost_border-radius_15 '>
+		<div class='lastPost_overflow lastPost_max_width lastPost_border_radius_20 $espaceurTop'>
 			<table class='lastPost_margin_bottom lastPost_w100 latstPost_responsive_table'>
 				<thead>
 					<tr>
-						<th>
-							<a href ='{$permalink}' title='{$post->post_title}' class='lastPost_text_deco_none lastPost_font_size_large'>{$post->post_title} </a>
+						<th colspan = 2 class='lastPost_backgroundColor_grisClair lastPost_text_align_left'>
+							<a href ='{$permalink}' title='{$post->post_title}' class='lastPost_text_deco_none lastPost_font_size_large'> {$post->post_title} </a>
 						</th>
 					</tr>
 				</thead>
-			</table>";
-		
+			";
+		// on verifie si l'article a une 1 image feature
 		if(has_post_thumbnail($post_id) !=''){
 			$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'thumbnail' );
-			$output = $output ."<table class='lastPost_margin_bottom lastPost_w100 latstPost_responsive_table'>
+			$output = $output ."
 			<tbody>
 				<tr>
 					<td class='lastPost_text_align_center lastPost_w30 '>
 						<img id='thumbnail' src='{$image[0]}' alt='image_thumbnail'/>
 					</td>";
 		}else {
+			// on verifie si l'article a au moins une 1 image
 			$firstImage = '';
 			ob_start();
 			ob_end_clean();
 			$outputImage = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
 			$firstImage = $matches [1];
+
 			if(empty($firstImage)){ 
 				// met en place une image par default si l'article n'en contient pas
 				$firstImage = plugin_dir_url(dirname(__FILE__)).'img/default.jpg';
-				$output = $output ."<table class='lastPost_margin_bottom lastPost_w100 latstPost_responsive_table'>
+				$output = $output ."
 				<tbody>
 					<tr>
 						<td class='lastPost_text_align_center lastPost_w30 '>
 							<img id='thumbnail' src='{$firstImage}' alt='image_thumbnail' class='lastPost_img_size'/>
 						</td>";
 			}else{
-				$output = $output ."<table class='lastPost_margin_bottom lastPost_w100 latstPost_responsive_table'>
+				$output = $output ."
 				<tbody>
 					<tr>
 						<td class='lastPost_text_align_center lastPost_w30 '>
@@ -62,23 +67,26 @@ function derniersArtilcesAccueil($atts){
 
 			}
 		}
-
+		// on recupère le contenu de l'article 
         $content = $post->post_content;
+		// on affiche qu'un resumé du texte
         $resumerContenu = substr($content, 0, 300);
 		$readMe = plugin_dir_url(dirname(__FILE__)).'img/open-book.png';
         $output = $output ." <td class='lastPost_w70'>{$resumerContenu}...</td>
 						</tr>
 						<tr>
 							<td colspan = 2 class='lastPost_text_align_right'>
-								<a href =\"window.location.href='{$permalink}';\" class='lastPost_text_deco_none'><img src='{$readMe}' alt='readMe' class='lastPost_img_icon_size'/> En savoir plus</a>
+								<a href =\"window.location.href='{$permalink}';\" class='lastPost_text_deco_none'><img src='{$readMe}' alt='readMe' class='lastPost_img_icon_size'/> En savoir plus... </a>
 							</td>
 						</tr>
 					</tbody>
 				</table>
 			</div>
 		</figure>";
-	}
 
+		$indiceEspaceur++;
+	}
+	// on redirige vers la page contenant tous les posts
 	$lienTousArticles = '?post_type=post';
 	$readAll = plugin_dir_url(dirname(__FILE__)).'img/book.png';
 	$output = $output ."<a href = '{$lienTousArticles}' class=''><img src='{$readAll}' alt='readAll' class='lastPost_img_icon_size'/> Voir les articles plus anciens</a>
